76adb01ee45334928160c6811d5b61b467d9b66d
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-03-29 11:05:12 +0100

    Update scr-inst-win.ps1

--- cdn/static/scr-inst-win.ps1
+++ cdn/static/scr-inst-win.ps1
@@ -1,3 +1,5 @@
+# Version: 1.0, Author: Simon Kalmi Claesson
+# THis file is meant to be executed easily from powershell using "iex(iwr -Uri "https://sbamboo.github.io/mcc-web/cdn/static/scr-inst-win.ps1" -UseBasicParsing).Content"
 # Define vars
 $folderPath = "C:\installs\scripted-install-temp"
 $bundleURL = "https://raw.githubusercontent.com/sbamboo/MinecraftCustomClient/main/v2/Installers/Builds/bundle.zip"
